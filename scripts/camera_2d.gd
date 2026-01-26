8cb209c8bef089dd2d5ae2a6b10a418b24532b78
ZeroPowerZero <sharmasusheel904@gmail.com>
2026-01-26 22:45:41 +0530

    angry push

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -1,22 +1,45 @@
 extends Camera2D
 
-var drag := false
+var draging := false
 var last_mouse_pos := Vector2.ZERO
+var block_pan := false
 
-func _unhandled_input(event):
+func _input(event):
+	
+	if event is InputEventMouseMotion:
+		
+		if event is InputEventMouseButton:
+			if event.button_index  == MOUSE_BUTTON_MIDDLE:
+				draging = true
+			else:
+				draging = false
+		
+		if draging:
+			print("hello dragg")
+			var _delta = get_global_mouse_position() - last_mouse_pos
+			position -= _delta * zoom
+			last_mouse_pos = get_global_mouse_position()
+		else :
+			last_mouse_pos = Vector2(0,0)
+			
 	# Middle mouse drag → pan
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_MIDDLE:
-			drag = event.pressed
-			last_mouse_pos = event.position
-
+	if event is InputEventMouseButton  :
+		#if event.button_index == MOUSE_BUTTON_MIDDLE:
+			#draging = event.pressed
+			#last_mouse_pos = event.position
+		
 		# Scroll → zoom
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+			
 			zoom *= 0.9
 		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
 			zoom *= 1.1
+	
 
-	if event is InputEventMouseMotion and drag:
-		var delta = event.position - last_mouse_pos
-		position -= delta * zoom
-		last_mouse_pos = event.position
+
+
+
+func _process(delta):
+	pass
+
+	
--- scripts/state.gd
+++ scripts/state.gd
@@ -1,9 +1,41 @@
 extends Node2D
 
 @export var state_name := "qo"
+
+var dragging := false
+var drag_offset := Vector2.ZERO
+
+func is_mouse_over() -> bool:
+	var mouse = to_local(get_global_mouse_position())
+	
+	return mouse.x > -40 and mouse.x < 40 and mouse.y > -40 and mouse.y < 40
+
+func _input(event):
+	print("hello")
+	#mouse button pressed on state
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		print("i am clicked")
+		
+			#check if click is on this state
+		if is_mouse_over():
+				dragging = true
+				drag_offset = global_position - get_global_mouse_position()
+				
+				
+		else:
+				dragging = false
+				
+				
+		if event is InputEventMouseMotion and dragging:
+			global_position = get_global_mouse_position() + drag_offset
+			
+	#get_viewport().set_input_as_handeled()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Label.text = state_name
+	get_tree().get_first_node_in_group("camera").block_pan = false
+	print(get_tree_string())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
